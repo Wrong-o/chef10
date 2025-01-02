87fb980cff8e04938bc8157b12ea2f202bcc5b1b
Wrong-o <otto@rongedal.se>
2025-01-02 03:56:19 +0100

    Grid style added

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 function showPopup(item) {
   //Set the message 
   document.getElementById('modalMessage').innerText = 'You clicked ' + item;
-
+  //Popup blocks everything
   document.getElementById('popupModal').style.display = 'block';
 }
 
+// Get the modal and the close button 
 var modal = document.getElementById('popupModal');
-var closeButton = document.getElementByClassName('close')[0];
-
+var closeButton = document.getElementsByClassName('close')[0];
+//Close modal with closeButtonj
 closeButton.onclick = function(){
   modal.style.display = 'none';
 }
 
+//Close if clickes outside
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = 'none'
-  }
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
 }
